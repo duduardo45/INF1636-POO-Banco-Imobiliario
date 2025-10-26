3f3dbef7868e0b2b66add05515e63fb1d5f1a8ae
pedro.nogueira <pedro.crnogueira@gmail.com>
2025-10-26 17:48:27 -0300

    ModelFacade criado e métodos básicos de Board

--- game/model/core/entities/Board.java
+++ game/model/core/entities/Board.java
@@ -2,7 +2,30 @@ package model.core.entities;
 
 import java.util.List;
 
-class Board {
+class Board { 
     private List<Space> all_spaces;
-    private List<Car> cars;
+    
+    Board(List<Space> spaces) { 
+        this.all_spaces = spaces;
+    }
+    
+    Space getSpaceAt(int index) { 
+        if (index >= 0 && index < all_spaces.size()) {
+            return all_spaces.get(index);
+        }
+        return null;
+    }
+    
+    int getTotalSpaces() { 
+        return all_spaces.size();
+    }
+    
+    int getPrisonPosition() { 
+        // Prisão está na posição 10 (baseado no tabuleiro padrão)
+        return 10;
+    }
+    
+    List<Space> getAllSpaces() { 
+        return all_spaces;
+    }
 }
--- game/model/core/entities/Start.java
+++ game/model/core/entities/Start.java
@@ -1,5 +1,19 @@
 package model.core.entities;
 
-class Start {
+public class Start extends Space {
+    private final int honorarium = 200;
     
+    public Start(String name, Space next) {
+        super(name, next);
+    }
+    
+    @Override
+    public void event(Player player) {
+        // Recebe $200 ao passar ou parar no Start
+        player.credit(honorarium);
+    }
+    
+    public int getHonorarium() {
+        return honorarium;
+    }
 }
